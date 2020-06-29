e8b408bed99a63e8ca4b4292656d46756f5894c9
imnotartsy <1664922+imnotartsy@users.noreply.github.com>
2020-06-29 13:38:08 -0400

    recovering lost work because of git pull overwrite?

---
+++ transformations/onshape_utils.py
@@ -0,0 +1,79 @@
+import api_utils as api
+
+#############################################
+#                                           #
+#             Assembly API Call             #
+#                                           #
+#############################################
+
+# getAssemblyInfo() - Calls 'assembly-definition' and returns a part and
+#      position list
+# Parameters:
+#   verbose - boolean for excessive print statements
+# Returns:
+#   - An assembly object (a list):
+#      - The first element is a part dictionary where the keys are part id's and
+#    the values are the names of the parts
+#      - The second element is a posision dictionary where the keys are part
+#   id's and the values are transformation matrices
+def getAssemblyInfo(verbose):
+    payload = {}
+    params = {}
+
+    response = api.callAPI('assembly-definition', {} , {})
+    print(response)
+
+    ### Creates Part List
+    parts = {}
+
+    # if (verbose):
+    #     print("Parts in assembly:")
+    # for instance in response["rootAssembly"]["instances"]:
+    #     if(verbose): print("  ", instance["id"], ":", instance["name"])
+    #     parts[instance["id"]] = instance["name"]
+    # if(verbose): print()
+
+
+    ### Gets current position
+    positions = {}
+
+    # # print("Positions of parts")
+    # for occurrence in response["rootAssembly"]["occurrences"]:
+    #     # print("  ", occurrence["path"][0],":", occurrence["transform"])
+    #     positions[occurrence["path"][0]] = occurrence["transform"]
+    # if(verbose): print()
+
+    return [parts, positions]
+
+
+# postTransform() - Calls 'occurence-transforms'
+# Parameters:
+#   M - a transform matrix
+#   parts - an array of part names to apply the transformation to
+#   relative - boolean for if the transform is relative (wip)  
+#   assembly - (as defined above in getAssemblyInfo) 
+#   verbose - boolean for excessive print statements
+# Returns:
+#   Nothing (success code/wip)
+def postTransform(M, isRelative, parts, assembly, verbose):
+    
+    payload = {}
+    # payload = {
+    #     "occurrences": [
+    #         {
+    #           "path": [
+    #             "MNjuuJ3b7llM9IQIb"
+    #           ]
+    #         }
+    #     ],
+    #     "transform": [                          
+    #         M
+    #     ],                          
+    #     "isRelative": true
+    # }
+    params = {}
+
+    # response = api.callAPI('assembly-definition', {} , {})
+    # print(json.dumps(response, indent = 2))
+
+    return "success"
