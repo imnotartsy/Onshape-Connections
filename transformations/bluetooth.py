ddab54f5e6946437a46c8b59aab85c776e3bdff4
imnotartsy <1664922+imnotartsy@users.noreply.github.com>
2020-07-06 21:20:26 -0400

    started python/terminal based IDE whoops

---
+++ transformations/bluetooth.py
@@ -0,0 +1,55 @@
+import serial #pip3 install pyserial
+import transform_utils # this is for the prompt user function
+
+
+### Connect to Serial 
+
+# TODO: automate this? (options:)
+# - list serials then select
+# - take it in as an arguement
+ser = serial.Serial('/dev/tty.LEGOHubOwen-SerialPortP')
+print(ser.name) 
+
+
+### Get Message
+
+# TODO: allow upload from a file
+# uploadFromFile = promptUser("Do you want to upload from a file?")
+# - will this allow for a refreshed file?
+# - auto place new lines and three returns
+
+# TODO: allow live upload from terminal
+# - read in input and automatically send with newlines and three returns
+# - how to deal with /r/n/r/n/r/n? (send every time? have an end of cmd "line")
+
+### Message to send to serial
+message = """
+from spike import ColorSensor\r\n
+from spike.control import wait_for_seconds\r\n
+import hub,utime\r\n
+
+scanner = ColorSensor('B')\r\n
+
+for i in range (0, 100):\r\n
+    # print("Scanner Red:"scanner.get_red())\r\n
+    # print("Scanner Green:"scanner.get_green())\r\n
+    # print("Scanner Blue:"scanner.get_blue())\r\n
+
+    print(hub.port.A.motor.get())\r\n ### [0] - rpm, [2] - position!
+wait_for_seconds(0.5)\r\n
+
+    \r\n\r\n\r\n\r\n
+"""
+
+print(message)
+ser.write(message.encode())
+
+### Read Data
+
+# TODO: figure out interupts with reading data and allowing new cmds
+
+for i in range(0,100):
+    line = ser.readline()   # read a '\n' terminated line
+    print(line.decode(), end="")
+
+ser.close()
