1e109538d18663eed081726ec4747c67bbd7d81d
imnotartsy <1664922+imnotartsy@users.noreply.github.com>
2020-07-14 15:21:49 -0400

    line wrapped to 80 col

--- transformations/utils/api_utils.py
+++ transformations/utils/api_utils.py
@@ -16,9 +16,13 @@ import argparse
 import json
 
 urls = {
-        'create-feature-studio': ['POST', '/api/featurestudios/d/did/w/wid'],
-        'assembly-definition': ['GET', '/api/assemblies/d/did/w/wid/e/eid'], # ?includeMateFeatures=OPT1&includeNonSolids=OPT2e&includeMateConnectors=OPT3
-        'occurrence-transforms': ['POST','/api/assemblies/d/did/w/wid/e/eid/occurrencetransforms'],
+        'create-feature-studio':
+            ['POST', '/api/featurestudios/d/did/w/wid'],
+        'assembly-definition':
+            ['GET', '/api/assemblies/d/did/w/wid/e/eid'],
+        # ?includeMateFeatures=OPT1&includeNonSolids=OPT2e&includeMateConnectors=OPT3
+        'occurrence-transforms':
+            ['POST','/api/assemblies/d/did/w/wid/e/eid/occurrencetransforms'],
 }
 
 #############################################
@@ -30,12 +34,17 @@ urls = {
 # Parse Arguements from the cmdline
 parser = argparse.ArgumentParser(description='Onshape API')
 
-parser.add_argument('-d', dest="did", help="Specify a document id (did) for your Onshape workspace")
-parser.add_argument('-w', dest="wid", help="Specify a workspace id (wid) for your Onshape workspace")
-parser.add_argument('-e', dest="eid", help="Specify an element id (eid) for your Onshape workspace")
-parser.add_argument('-b', dest="base", help="Specify a base url your Onshape workspace")
+parser.add_argument('-d', dest="did",
+    help="Specify a document id (did) for your Onshape workspace")
+parser.add_argument('-w', dest="wid",
+    help="Specify a workspace id (wid) for your Onshape workspace")
+parser.add_argument('-e', dest="eid",
+    help="Specify an element id (eid) for your Onshape workspace")
+parser.add_argument('-b', dest="base",
+    help="Specify a base url your Onshape workspace")
 
-parser.add_argument('-p', dest="port", help="Specify a port for your Spike Prime")
+parser.add_argument('-p', dest="port",
+    help="Specify a port for your Spike Prime")
 
 args = parser.parse_args()
 
@@ -48,7 +57,8 @@ if (not (args.did and args.wid and args.eid)):
             args.wid  = f.readline().rstrip()
             args.eid  = f.readline().rstrip()
     except:
-        print("All parameters not given. Please give did, wid, and eid using the flags -d, -w, and -e")
+        print("All parameters not given.")
+        print("Please give did, wid, and eid using the flags -d, -w, and -e")
         exit()
 
 if (not args.base):
@@ -61,8 +71,11 @@ with open("api-key", "r") as f:
     secret = f.readline().rstrip()
 
 # Setting up the client
-client = Client(configuration={"base_url": args.base, "access_key": key, "secret_key": secret})
-headers = {'Accept': 'application/vnd.onshape.v1+json; charset=UTF-8;qs=0.1', 'Content-Type': 'application/json'}
+client = Client(configuration={"base_url": args.base,
+                               "access_key": key,
+                               "secret_key": secret})
+headers = {'Accept': 'application/vnd.onshape.v1+json; charset=UTF-8;qs=0.1',
+           'Content-Type': 'application/json'}
 
 
 # checkArgs() - Validates the did, wid, and eid
@@ -107,8 +120,8 @@ def callAPI(endpoint, params, payload, hasReturn):
     #   fixed_url = fixed_url.replace('OPT2', "true") # Non Solids
     #   fixed_url = fixed_url.replace('OPT3', "true") # Mate Connectors
 
-    response = client.api_client.request(method,
-        url=args.base + fixed_url, query_params=params, headers=headers, body=payload)
+    response = client.api_client.request(method, url=args.base + fixed_url,
+        query_params=params, headers=headers, body=payload)
 
     # print(response.data)
     if (hasReturn):
